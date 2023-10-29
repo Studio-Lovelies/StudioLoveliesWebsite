e40b65be6ac2fcd362f7be9c927e6416353041e8
Vincent Mahnke <vincent@mahn.ke>
2023-10-29 23:47:12 +0000

    fix: Removes absolute path specification

--- build.js
+++ build.js
@@ -1,8 +1,7 @@
 import { execSync } from 'child_process';
 import { writeFileSync } from 'fs';
-import { join } from 'path';
 
-const versionFile = join(__dirname, 'public/views/version.html');
+const versionFile = 'public/views/version.html';
 const version = execSync('git rev-parse HEAD').toString().trim();
 
 writeFileSync(versionFile, version);
