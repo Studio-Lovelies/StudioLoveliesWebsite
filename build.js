b9cd2cdcf89b5c165b5ffe732d485738eca94bb1
Vincent Mahnke <vincent@mahn.ke>
2023-10-29 23:44:46 +0000

    fix: Converts to ES module syntax

--- build.js
+++ build.js
@@ -1,6 +1,6 @@
-const { execSync } = require('child_process');
-const { writeFileSync } = require('fs');
-const { join } = require('path');
+import { execSync } from 'child_process';
+import { writeFileSync } from 'fs';
+import { join } from 'path';
 
 const versionFile = join(__dirname, 'public/views/version.html');
 const version = execSync('git rev-parse HEAD').toString().trim();
